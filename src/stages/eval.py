d20ed06ce9611adc26c1b1928172ac7e56c641c4
Alexander Kim <>
2022-02-18 16:40:10 -0500

    fix indentation
    add AWS_SESSION_TOKEN
    
    update cml config
    
    change instance to g3s.xlarge
    
    remove --cloud-spot
    
    set PYTHONPATH
    
    update
    
    update
    
    update
    
    echo github path
    
    update
    
    update
    
    update
    
    upd
    
    upd
    
    upd github.workspac
    
    add step "Set PYTHONPATH"
    
    update
    
    update cml.yaml
    
    test aws
    
    add dvc pull
    
    fix aws creds
    
    upd
    
    upd
    
    update report.md
    
    change instance to g5.xlarge
    
    upd
    
    change instance to p3.2xlarge

--- src/stages/eval.py
+++ src/stages/eval.py
@@ -8,13 +8,12 @@ from src.load_params import load_params
 
 def evaluate(params_path):
     params = load_params(params_path)
+    data_dir = Path(params.data_load.data_dir)
     test_img_dir_path = Path(params.data_split.test_img_dir_path)
     test_mask_dir_path = Path(params.data_split.test_mask_dir_path)
-    reports_dir_path = Path(params.base.reports_dir_path)
-    reports_dir_path.mkdir(exist_ok=True)
-    img_out_dir = reports_dir_path/'test_preds'
-    img_out_dir.mkdir(exist_ok=True)
-    metrics_file_path = reports_dir_path/params.evaluate.metrics_file
+    test_img_out_dir = data_dir/'test_preds'
+    test_img_out_dir.mkdir(exist_ok=True)
+    metrics_file_path = params.evaluate.metrics_file
     save_test_preds = params.evaluate.save_test_preds
     img_size = params.train.img_size
     model_pickle_dir_path = Path(params.train.model_pickle_dir_path)
@@ -23,7 +22,7 @@ def evaluate(params_path):
     metrics = get_metrics(test_img_dir_path=test_img_dir_path,
                           test_mask_dir_path=test_mask_dir_path,
                           model_pickle_path=model_pickle_path,
-                          img_out_dir=img_out_dir,
+                          test_img_out_dir=test_img_out_dir,
                           img_size=img_size,
                           save_test_preds=save_test_preds)
     json.dump(
